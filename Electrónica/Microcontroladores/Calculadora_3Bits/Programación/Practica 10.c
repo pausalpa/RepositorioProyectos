f1c36c4b7a6942f5fe4b2d9afed3e2aca7e73ee0
Paulo Salvatore <paulo.salvatore.9@gmail.com>
2022-06-23 22:52:10 -0500

    Adición de sección PIC con primer proyecto
    Incluye la Calculadora de 3Bits

---
+++ Electrónica/Microcontroladores/Calculadora_3Bits/Programación/Practica 10.c
@@ -0,0 +1,58 @@
+/* Práctica 10: Calculadora de 3 bits
+   Paulo Salvatore Partida Escamilla   15300210
+   Jose Luis Marín Alarcón             15300157
+*/
+#include <16F886.h>
+#use delay(clock=4000000)
+#include <lcd1.c>
+#fuses intrc_io,nowdt,noput,nomclr,noprotect,nocpd,nobrownout
+#fuses noieso,nofcmen,nodebug,nowrt,borv21
+#use fast_io(A)
+#use fast_io(B)
+
+void main()
+{     
+   set_tris_A(0xFF);
+   lcd_init(); 
+   while(1)
+   {
+      int A=Input_A();                                                             //0011 1000= 38
+      A=A&0x38;
+      A>>=3;
+      
+      int B=Input_A();                                                             //0000 0111 =07
+      B=B&0x07;
+   
+      int Operacion=Input_A();                                                     //1100 0000= C0
+      Operacion=Operacion&0xC0;
+      Operacion>>=6;
+ 
+      signed int Resultadosrm;
+      Lcd_gotoxy(1,1);
+      if (Operacion== 0x00)                                                      //Suma
+         {
+         Resultadosrm=A+B;
+         printf(lcd_putc,"\f \n \f");
+         printf (lcd_putc, "A + B\n %u + %u = %u",A,B,Resultadosrm);
+         }
+      if (Operacion== 0x01)                                                      //Resta
+         {
+         Resultadosrm=A-B;
+         printf(lcd_putc,"\f \n \f");
+         printf (lcd_putc, "A - B\n %u - %u = %d",A,B,Resultadosrm);
+         }
+      if (Operacion== 0x02)                                                      //Multiplicación
+         {
+         Resultadosrm=A*B;
+         printf(lcd_putc,"\f \n \f");
+         printf (lcd_putc, "A * B\n %u * %u = %d",A,B,Resultadosrm);
+         }
+      if (Operacion== 0x03)                                                      //División
+         {
+         float Resultadod=(float)A/B;
+         printf (lcd_putc,"\f \n \f");
+         printf (lcd_putc, "A / B\n %u / %u = %.3g",A,B,Resultadod);
+         }
+      delay_ms(300);
+   }
+}
